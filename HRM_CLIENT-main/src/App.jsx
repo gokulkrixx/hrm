807e260989b0bee0ac03816513b1a661619234cc
gokul <gk503255@gmail.com>
2024-03-26 11:49:42 +0530

    changes in client and server

--- HRM_CLIENT-main/src/App.jsx
+++ HRM_CLIENT-main/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
     
-
+{/* Some changes */}
   <Landing/>
   {/* < Adminlogin/> */}
   {/* <Adduser/> */}
--- hrm-server/server.js
+++ hrm-server/server.js
@@ -13,7 +13,7 @@ app.get('/test', (req, res) => {
     res.send('success');
   });
 
-
+//Some changes
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
